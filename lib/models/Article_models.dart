5379449bf54598a235b8baa11d54d169c2b419a8
MustafaSabry_1 <promostafa23@gmail.com>
2026-02-28 17:37:02 +0200

    Add factory constructor for ArticleModels from JSON
    Added a factory constructor to create ArticleModels from JSON data.

--- lib/models/Article_models.dart
+++ lib/models/Article_models.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 class ArticleModels {
   final String? image;
   final String title;
@@ -7,5 +9,14 @@ class ArticleModels {
     required this.image,
     required this.title,
     required this.subtitle,
+    
   });
+  factory ArticleModels.fromJson(json){
+    return ArticleModels(
+          image: json["urlToImage"],
+          title: json["title"],
+          subtitle: json ["description"],
+        );
+   } 
+   
 }
